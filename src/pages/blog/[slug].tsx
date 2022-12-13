69efb5b0a3e8d4107621825aa867cbebd8b5be34
Chukwuma Okoroji <chuck@Chucks-MBP.lan>
2022-12-12 19:30:34 -0500

    added next-mdx-remote and gray-matter

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface BlogPostProps {
+    title: string
+    description: string
+    author: string
+    body: string
+}
+
+export const BlogPost: React.FC = () => {
+    return (
+        <div className='flex min-h-screen w-full items-center justify-center'>
+
+        </div>
+    )
+}
