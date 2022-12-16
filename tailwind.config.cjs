3d8b5884235d1c778867db2a9969a34baa7b8d66
Chukwuma Okoroji <chuck@Chucks-MBP.lan>
2022-12-16 09:54:25 -0500

    removed prisma, installed next-mdx, and created posts subdir

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          550: '#ef233c',
+          650: '#d90429'
+        },
+        gray: {
+          450: '#8d99ae'
+        }
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: '#edf2f4',
+      gray: colors.slate,
+      yellow: colors.amber,
+      green: colors.emerald,
+      pink: colors.rose,
+      purple: colors.purple,
+      red: colors.red,
+      blue: colors.blue,
+    }
   },
   plugins: [],
 };
