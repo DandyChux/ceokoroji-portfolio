0151b27d93228b75a59057bc49d7f40cdc8838c3
Chuck Okoroji <dandychux@gmail.com>
2023-05-22 11:37:07 -0400

    removed contentlayer from gitignore

---
+++ .contentlayer/.cache/v0.3.2/compiled-contentlayer-config-C7EILP2W.mjs
@@ -0,0 +1,85 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    date: {
+      type: "date",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    published: {
+      type: "boolean",
+      default: true
+    },
+    tags: {
+      type: "list",
+      of: {
+        type: "string"
+      }
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (post) => `/${post._raw.flattenedPath}`
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (post) => post._raw.flattenedPath
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section"
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-C7EILP2W.mjs.map
