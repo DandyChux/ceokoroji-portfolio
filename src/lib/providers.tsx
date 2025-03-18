cb5808ee23ad5e11c7a5bf29f3570056fcf21dee
Chuck Okoroji <dandychux@gmail.com>
2025-03-18 15:33:04 -0400

    added plausible tracking

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -7,23 +7,26 @@ import { AlertProvider } from '~/contexts/alert-context'
 import { AppProvider } from '~/contexts/app-context'
 import { ModalProvider } from '~/contexts/modal-context'
 import { ViewportProvider } from '~hooks/useViewport'
+import PlausibleProvider from "next-plausible"
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-    const [queryClient] = useState(() => new QueryClient({}))
+	const [queryClient] = useState(() => new QueryClient({}))
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AppProvider>
-                <ViewportProvider>
-                    <TooltipProvider>
-                        <AlertProvider>
-                            <ModalProvider>
-                                {children}
-                            </ModalProvider>
-                        </AlertProvider>
-                    </TooltipProvider>
-                </ViewportProvider>
-            </AppProvider>
-        </QueryClientProvider>
-    )
+	return (
+		<PlausibleProvider domain="ceokoroji.dev" customDomain="https://plausible.blackstacksolutions.com" selfHosted>
+			<QueryClientProvider client={queryClient}>
+				<AppProvider>
+					<ViewportProvider>
+						<TooltipProvider>
+							<AlertProvider>
+								<ModalProvider>
+									{children}
+								</ModalProvider>
+							</AlertProvider>
+						</TooltipProvider>
+					</ViewportProvider>
+				</AppProvider>
+			</QueryClientProvider>
+		</PlausibleProvider>
+	)
 }
