9250f4afb43aae4625b07f744f86b0670f5b0a66
Chuck Okoroji <dandychux@gmail.com>
2025-12-16 18:55:03 -0500

    refactor: share links

--- src/components/share-links.svelte
+++ src/components/share-links.svelte
@@ -18,26 +18,6 @@
 	const encodedUrl = encodeURIComponent(page.url.href);
 	const encodedTitle = encodeURIComponent(title);
 	const encodedDescription = encodeURIComponent(description);
-
-	const shareOnTwitter = () => {
-		const url = `https://twitter.com/intent/tweet?text=${encodedTitle}${page.url.href}`;
-		window.open(url, "_blank");
-	};
-
-	const shareOnFacebook = () => {
-		const url = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
-		window.open(url, "_blank");
-	};
-
-	const shareOnLinkedIn = () => {
-		const url = `https://www.linkedin.com/feed/?shareActive=true&shareUrl=${encodedUrl}`;
-		window.open(url, "_blank");
-	};
-
-	const shareOnInstagram = () => {
-		const url = `https://www.instagram.com/share?url=${encodedUrl}`;
-		window.open(url, "_blank");
-	};
 </script>
 
 <div
@@ -52,7 +32,7 @@
 	</span>
 	<Button
 		variant="link"
-		href="https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedTitle}"
+		href="https://twitter.com/intent/tweet?url=${encodedUrl}&text=${title}"
 		target="_blank"
 		rel="noopener noreferrer"
 		size="sm"
@@ -80,7 +60,7 @@
 	</Button>
 	<Button
 		variant="link"
-		href=""
+		href="https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}"
 		target="_blank"
 		rel="noopener noreferrer"
 		size="sm"
@@ -88,7 +68,6 @@
 			variant: "outline",
 			class: "rounded-full cursor-pointer",
 		})}
-		onclick={shareOnFacebook}
 	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -109,8 +88,7 @@
 	</Button>
 	<Button
 		variant="link"
-		href="https://www.linkedin.com/shareArticle?mini=true&url=${encodedUrl}&title=${encodedTitle}&summary=${encodedDescription}&source=${page
-			.url.host}"
+		href="https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}"
 		target="_blank"
 		rel="noopener noreferrer"
 		size="sm"
@@ -118,7 +96,6 @@
 			variant: "outline",
 			class: "rounded-full cursor-pointer",
 		})}
-		onclick={shareOnLinkedIn}
 	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -140,36 +117,5 @@
 			/></svg
 		>
 		<span class="sr-only">Share on LinkedIn</span>
-	</Button>
-	<Button
-		variant="link"
-		href=""
-		target="_blank"
-		rel="noopener noreferrer"
-		size="sm"
-		class={buttonVariants({
-			variant: "outline",
-			class: "rounded-full cursor-pointer",
-		})}
-		onclick={shareOnInstagram}
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
-			><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-				d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"
-			/><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path
-				d="M16.5 7.5v.01"
-			/></svg
-		>
-		<span class="sr-only">Share on Instagram</span>
 	</Button>
 </div>
