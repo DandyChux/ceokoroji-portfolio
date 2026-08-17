8b541f22db572a50e16550b4b26d6e6ed13c7b17
Chuck Okoroji <dandychux@gmail.com>
2026-08-17 12:40:42 -0400

    perf(ui): optimize logo loading and preconnects

--- ui/src/components/logo.svelte
+++ ui/src/components/logo.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
 	import { page } from "$app/state";
+	import { generateSrcSet } from "$lib/utils";
+	import Picture from "./picture.svelte";
 	import Button, { buttonVariants } from "./ui/button/button.svelte";
 
 	let pathname = page.url.pathname;
 </script>
 
 <div class="hidden xl:flex md:flex-col absolute top-4 right-4 items-center">
-	<Button variant="link" href="/admin" class="cursor-default">
-		<enhanced:img
+	<Button
+		variant="link"
+		href="/admin"
+		class="cursor-default"
+		data-sveltekit-preload-data="tap"
+	>
+		<Picture
 			src={"/ceokoroji-logo.webp"}
 			alt="Logo"
 			width="100"
 			height="100"
+			sizes="175px"
+			srcset={generateSrcSet("/ceokoroji-logo.webp", [175], "webp", 85)}
 		/>
 	</Button>
 </div>
